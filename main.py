4f9bc27e84a1a07b96cbda988841cf37bdf8a80e
SammyBooyyy <69029079+SammyBooyyy@users.noreply.github.com>
2021-02-18 13:23:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ pc = ['pedra', 'papel', 'tesoura']
 escolhas_pc = (random.choice(pc))
 escolhas_user = input('Escolhe um dos objetos: pedra, papel ou tesoura > ')
 
-if escolhas_pc == "user":
-	print('O pc está a pensar, por favor espera:')
+if escolhas_pc == escolhas_user:
+	print('O pc está a pensar, por favor espera')
 	time.sleep(3)
 	print(f'Tu escolheste {escolhas_user} e o pc também escolheu {escolhas_pc}, então é um impate')
 elif escolhas_user == "pedra":
-	print('O pc está a pensar, por favor espera:')
+	print('O pc está a pensar, por favor espera')
 	time.sleep(3)
 	if escolhas_pc == "tesoura":
 		print(f'Tu escolheste {escolhas_user} e o pc escolheu {escolhas_pc} então torna-te a ti o vencedor')
 	else:
 		print(f'O PC ganhou de ti, tu escolheste {escolhas_user} e o pc escolheu {escolhas_pc} ')
 elif escolhas_user == "tesoura":
-	print('O pc está a pensar, por favor espera:')
+	print('O pc está a pensar, por favor espera')
 	time.sleep(3)
 	if escolhas_pc == "papel":
 		print(f'Tu escolheste {escolhas_user} e o pc escolheu {escolhas_pc} então torna-te a ti o vencedor')
 	else:
 		print(f'O PC ganhou de ti, tu escolheste {escolhas_user} e o pc escolheu {escolhas_pc} ')
 else:
-	print('O pc está a pensar, por favor espera:')
+	print('O pc está a pensar, por favor espera')
 	time.sleep(3)
-	if escolhas_pc == "tesoura":
+	if escolhas_pc == "pedra":
 		print(f'Tu escolheste {escolhas_user} e o pc escolheu {escolhas_pc} então torna-te a ti o vencedor')
 	else:
 		print(f'O PC ganhou de ti, tu escolheste {escolhas_user} e o pc escolheu {escolhas_pc} ')
